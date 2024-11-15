5ee93fdc73c7505211deb65d290fbe7446f96285
Kristoffer Lindström <kristoffer@workera.ai>
2024-11-15 16:50:50 +0100

    chore: include spec definitions for new util functions

--- lib/samly/state/state_util.ex
+++ lib/samly/state/state_util.ex
@@ -3,10 +3,12 @@ defmodule Samly.State.StateUtil do
 
   alias Samly.Assertion
 
+  @spec validate_login_assertion_expiry(Assertion.t()) :: :valid | :expired
   def validate_login_assertion_expiry(%Assertion{subject: %{notonorafter: expiry_date}}) do
     if date_passed?(expiry_date), do: :expired, else: :valid
   end
 
+  @spec validate_logout_assertion_expiry(Assertion.t()) :: :valid | :expired
   def validate_logout_assertion_expiry(%Assertion{authn: authn, subject: subject}) do
     expiry_date =
       Map.get(authn, "session_not_on_or_after", subject.notonorafter)
